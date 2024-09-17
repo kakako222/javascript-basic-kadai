a459fb8a3d2311579d7d629f593deb085237ed50
shibacaco <shibacaco@MiyachinoMacBook-Air.local>
2024-09-17 18:13:30 +0900

    kadai_021

---
+++ kadai_021/event.js
@@ -0,0 +1,20 @@
+//btnというidを持つHTML要素を取得し、定数に代入する
+const btn = document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
+
+//HTML要素がクリックされた時にイベントを開始する
+btn.addEventListener('click', () => {
+
+    //要素を新しく作成する
+    const childList = document.createElement('li');
+
+    //作成したli要素に""これはリスト要素です"というテキストを追加する
+    setTimeout(() => {
+        childList.textContent = 'ボタンをクリックしました';
+        text.textContent = 'ボタンをクリックしました';
+
+    }, 2000);
+
+});
